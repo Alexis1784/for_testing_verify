3923d6fe5b7997e25d0e2be9d68057fef9cc57e0
Alex <alexis1784@gmail.com>
2015-12-27 05:34:36 +0500

    changing HomeController Index method

--- lesson18_4_finish/Controllers/HomeController.cs
+++ lesson18_4_finish/Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using lesson18_4_finish.Models;
 
 namespace lesson18_4_finish.Controllers
 {
     public class HomeController : Controller
     {
+        CompContext db = new CompContext();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Computers);
         }
 
         public ActionResult About()
